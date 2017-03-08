7e49c98de852d5de63f22ab8a9591c53022e43c1
Colin Hart <me@colin-hart.com>
2017-03-08 14:00:25 -0500

    Removed solution

--- solution/gladiator.rb
+++
@@ -1,7 +0,0 @@
-class Gladiator
-  attr_reader :name, :weapon
-  def initialize(name, weapon)
-    @name = name
-    @weapon = weapon.downcase
-  end
-end
--- solution/main.rb
+++
@@ -1,2 +0,0 @@
-require_relative 'arena.rb'
-require_relative 'gladiator.rb'
